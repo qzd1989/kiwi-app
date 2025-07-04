778a77ca0a2130d625d071806548ea957e7afcbc
kiwi <1145567334@qq.com>
2025-07-05 02:24:22 +0800

    1

--- src-tauri/assets/python/project_template/main.py
+++ src-tauri/assets/python/project_template/main.py
@@ -2,9 +2,8 @@ import time
 from kiwi import ScreenClient, Point, RelativePoint, RgbOffset, System, Key
 
 client = ScreenClient()
-system = System()
 
 while True:
     data = client.get_mouse_location().data
     print("location:", data, data.x, data.y)
-    system.sleep(milliseconds=1000)
+    System.sleep(milliseconds=1000)
